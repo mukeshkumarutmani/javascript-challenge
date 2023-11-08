278ae9e586cdabb9f5ed6b48028bcab4dc3f98ad
Mukesh Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-11-08 07:25:35 -0800

    Update closure_1stday.js

--- closure_1stday.js
+++ closure_1stday.js
@@ -12,9 +12,28 @@ console.log(result)
 
 // counter function by using closure pattern
 
+function createfunction(a){
+    let count=a;
+    let isFirstCall = true;
 
+     function counter(){
+        if (isFirstCall) {
+        isFirstCall = false;
+        return count; // Return the initial value 'n' on the first call
+    } else {
+        count++; // Increment the count for subsequent calls
+        return count; // Return the incremented value
+    }
+  }
+    return counter;
 
-// const n=5;
-// const result=createCounter(n);
+}
+const n=10;
+const counter=createfunction(n);
+// console.log(n)
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
 
 
