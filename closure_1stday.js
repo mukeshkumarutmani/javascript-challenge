3f8caf80fca0f02d5d41d466f2ec7e894809aa24
Mukesh Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-11-09 09:38:59 -0800

    Update closure_1stday.js

--- closure_1stday.js
+++ closure_1stday.js
@@ -79,3 +79,25 @@ const assertion=exert(value);
  }
  
 
+
+function createCount(init){
+    let count =init;
+  return {
+  increment: function(){
+    count++;
+    return count
+  },
+  
+  decrement: function(){
+      count--;
+      return count;
+  },
+
+  reset: function(){
+     count=init;
+     return count;
+  }
+
+  }
+}
+
